06660f7c83d9ff7d7662d713534bf88590164d3b
DBagrecha <88670646+DBagrecha@users.noreply.github.com>
2023-02-12 20:41:24 +0530

    Update AffinityAnswers.py

--- AffinityAnswers.py
+++ AffinityAnswers.py
@@ -10,16 +10,17 @@ for w in f:
   word_list.add(w.strip())
 
 # Assuming that the tweets are in the the column "tweets"
-# Making a new column "profanity" in DataFrame which will have the profanity values
 # Calculating profanity using the formula: racial slurs / total words in tweet
 
 arr=[]
 # Looping through each tweet
 for tweet in df['tweets']:
   words = 0
+  # Looping through each word
   for w in word_tokenize(tweet):
     if w in word_list:
       words+=1
   arr.append(words/len(tweet))
 
+# Making a new column "profanity" in DataFrame which will have the profanity values
 df['profanity']=arr
